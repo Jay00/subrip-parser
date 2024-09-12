af70ce4c39b1da1daf6bc9b9bd1c2aed4b0bd124
Jason Clark <jasonclarklaw@gmail.com>
2024-09-12 09:55:46 -0400

    not working

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::fs;
+
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "srt.pest"]
+pub struct CSVParser;
+
+fn main() {
+    let unparsed_file = fs::read_to_string("sample.srt").expect("cannot read file");
+
+    print!("{}", unparsed_file);
+
+    let file = CSVParser::parse(Rule::file, &unparsed_file)
+        .expect("unsuccessful parse") // unwrap the parse result
+        .next()
+        .unwrap(); // get and unwrap the `file` rule; never fails
+
+    // let successful_parse = CSVParser::parse(srt);
+    // println!("{:?}", successful_parse);
+
+    // let unsuccessful_parse = CSVParser::parse(Rule::field, "this is not a number");
+    // println!("{:?}", unsuccessful_parse);
+}
